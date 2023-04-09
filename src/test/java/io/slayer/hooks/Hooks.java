ebea284dfebc308fdc3816581db29830676926b7
Dmitrii Vartik <dekesite@mail.ru>
2023-04-09 22:28:39 +0300

    added execution order for cucumber hooks

--- src/test/java/io/slayer/hooks/Hooks.java
+++ src/test/java/io/slayer/hooks/Hooks.java
@@ -8,15 +8,30 @@ import cucumber.api.java.BeforeStep;
 public class Hooks {
 
     // run before each scenario
+    // default order = 10000
+    // with @Before method with smaller order is executed first
+    // 1 (first), 10, 15, 100, 1000, 10000 (last)
     @Before
+    public void prepareData() {
+        System.out.println("Данные в БД добавлены");
+    }
+
+    @Before(order = 1000)
     public void connectToBD() {
         System.out.println("Подключение к БД установлено");
     }
 
     // run after each scenario
-    @After
+    // with @After method with bigger order is executed first
+    // 10000 (first), 2000, 150, 10, 1 (last)
+    @After(order = 11000)
     public void clearData() {
         System.out.println("Данные очищены");
+    }
+
+    @After
+    public void bdDisconnect() {
+        System.out.println("Подключение к БД остановлено");
     }
 
     // run before each step
